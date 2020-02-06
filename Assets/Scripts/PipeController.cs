f7579dbd6330e2d0c2ac474a064d9de6af868987
RECNEPS-KIPS <2432719656@qq.com>
2020-02-06 20:36:53 +0800

    实现Pipe的Y坐标在固定范围内随机变化
    UnityEngine.Random.Range(min,max);返回float类型数值

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            float r= UnityEngine.Random.Range(-0.4f, -0.1f);//生成随机数
+            transform.position = new Vector3(transform.position.x, 15*r,transform.position.z);//修改Y坐标
+        }
+    }
+}
