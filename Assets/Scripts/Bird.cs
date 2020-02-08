53d49b24b537f8a7c1369f2529213aada9694c33
RECNEPS-KIPS <2432719656@qq.com>
2020-02-08 15:29:06 +0800

    实现游戏状态切换

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
-public class Bird : MonoBehaviour
-{
+
+public class Bird : MonoBehaviour {
     public Material mat;
     public int frameCount;
 
@@ -13,25 +13,33 @@ public class Bird : MonoBehaviour
     private float temp;
 
     public Rigidbody rigid;
-    public event Action KnockTrap;//声明一个事件,发布消息
-    public CameraController cc;
-    private bool dead;
-    
+    //public event Action KnockTrap; //声明一个事件,发布消息
+    //public CameraController cc;
+
+    public GameManager.GameState state;
+
     // Start is called before the first frame update
     void Start() {
+        //设置默认状态
+        state = GameManager._instance.currentState;
         mat = GetComponent<Renderer>().material;
         frameCount = 0;
         temp = 0;
         rigid = GetComponent<Rigidbody>();
         //cc = Camera.main.GetComponent<CameraController>();
-        
-        dead = false;
     }
 
     // Update is called once per frame
     void Update() {
+        state = GameManager._instance.currentState;
         Vector3 v = rigid.velocity;
-        if (!dead) {
+        //菜单状态
+        if (state == GameManager.GameState.MENU) {
+            rigid.useGravity = false;
+        }
+        //游戏中
+        if (state == GameManager.GameState.RUNNING) {
+            rigid.useGravity = true;
             rigid.velocity = new Vector3(5, v.y, 0);//添加水平速度
             //Animation
             mat.SetTextureOffset(1, new Vector2(frameCount * 1 / 3.0f, 0));
@@ -41,25 +49,29 @@ public class Bird : MonoBehaviour
                 temp = 0;
             }
             frameCount %= 3;
+
+            //Controller
+            if (Input.GetMouseButtonDown(0)) {
+                rigid.velocity = new Vector3(v.x, 5, 0);
+            }
         }
-        
-        //Controller
-        if (Input.GetMouseButtonDown(0)&&!dead) {
-            rigid.velocity = new Vector3(v.x, 5, 0);
-        }
+
+
     }
 
     //发生碰撞
     void OnCollisionEnter(Collision other) {
-        if (other.collider.tag=="Trap") {
-            //KnockTrap += cc.StopMove;
+        if (other.collider.tag == "Trap") {
+            //KnockTrap += cc.StopMove;//注册方法
             //KnockTrap?.Invoke();//不为NULL则调用
-            dead = true;//将bird置为dead状态
+            //将状态设置为END
+            GameManager._instance.SetState(GameManager.GameState.END);
         }
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.tag=="Score"&&!dead) {
+
+        if (other.tag == "Score") {
             GameManager._instance.score++;
             GameObject.Find("Score").transform.GetComponent<Text>().text = "Your Score : " + GameManager._instance.score;
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,33 @@ public class GameManager : MonoBehaviour {
     public Transform firstBg;//当前状态最后一个Bg
     public static GameManager _instance;
     internal int score;
+
+    public enum GameState {
+        MENU,
+        RUNNING,
+        END
+    }
+    public GameState currentState;//当前游戏状态
     //单例模式
     void Awake() {
+        currentState = GameState.MENU;
         _instance = this;
         firstBg = GameObject.Find("bg5").transform;
         score = 0;
     }
     // Start is called before the first frame update
     void Start() {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.anyKey&&currentState!=GameState.END) {
+            currentState = GameState.RUNNING;
+        }
+    }
 
+    public void SetState(GameState state) {
+        currentState = state;
     }
 }
